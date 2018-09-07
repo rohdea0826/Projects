91be26e8645b790ecc6679a363474e1076b55842
rohdea0826 <sqg0826@foxmail.com>
2018-09-07 10:16:52 +0800

    chatServer_Linux

---
+++ myChatSystem/chatsystem/client/udp_client.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <jsoncpp/json/json.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+
+#define MAX 1024
+class client{
+	public:
+		client(std::string _ip, int _port);
+		void clientInit();
+		void recvData(std::string &out);
+		void sendData(const std::string &in);
+		~client();
+
+	private:
+		int sock;
+		struct sockaddr_in server;
+};
+
+
+
+
+
+
+
+
---
+++ myChatSystem/chatsystem/server/udp_server.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <jsoncpp/json/json.h>
+#include "data_pool.h"
+#include "data.h"
+
+#define MAX 1024
+
+class server{
+	public:
+		server(int _port);
+		void serverInit();
+		void recvData(std::string &out);
+		void sendData(const std::string &in, struct sockaddr_in &peer);
+		void broadcastData();
+		~server();
+
+	private:
+		int sock;
+		int port;
+		dataPool pool;
+		std::map<uint32_t, struct sockaddr_in> online;
+};
+
+
+
+
+
+
+
+
+
